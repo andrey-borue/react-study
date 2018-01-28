4ef5d01feb6fd970c6ba57f1aaee042a2813e5ab
Andrey Borue <andrey.borue@gmail.com>
2018-01-28 15:35:38 +0200

    Events AuthForm

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const AuthForm = ({onAuth}) => {
+  const user = [];
+
+  const onSubmit = event => {
+    event.preventDefault();
+    typeof onAuth === 'function' ? onAuth(user) : console.error('onAuth не является функцией');
+  };
+
+  const name = (
+      <div className="Input">
+        <input required type="text" placeholder="Имя" onChange={event => user.name = event.currentTarget.value}/>
+        <label/>
+      </div>
+  );
+
+  const password =  (
+      <div className="Input">
+        <input required type="password" placeholder="Пароль" onChange={event => {user.password = event.currentTarget.value = event.currentTarget.value.replace(/[^a-zA-Z0-9_]/, '')}} />
+        <label />
+      </div>
+  );
+
+  const email = (
+    <div className="Input">
+      <input type="email" placeholder="Электронная почта" onChange={event => {user.email = event.currentTarget.value = event.currentTarget.value.replace(/[^a-zA-Z0-9_@\.\-]/, '')}} />
+        <label />
+    </div>
+  );
+  
+  return (
+    <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={onSubmit}>
+      {name}
+      {email}
+      {password}
+      <button type="submit">
+        <span>Войти</span>
+        <i className="fa fa-fw fa-chevron-right" />
+      </button>
+    </form>
+  )
+};
