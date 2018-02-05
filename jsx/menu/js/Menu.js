0b5bc07a0b011bcfc5ed10584b763b252f5d3c00
Andrey Borue <andrey.borue@gmail.com>
2018-02-05 19:03:51 +0200

    fix "menu-open" class

--- jsx/menu/js/Menu.js
+++ jsx/menu/js/Menu.js
@@ -2,7 +2,7 @@ const Menu = function ({opened, items}) {
     const list = items.map((item, index) => <li key={index}><a href={ item.href }>{item.title}</a></li>);
 
     return (
-       <div className="menu menu-open">
+       <div className={"menu " + (opened ? "menu-open" : "") }>
          <div className="menu-toggle"><span /></div>
          { opened && (
            <nav>
