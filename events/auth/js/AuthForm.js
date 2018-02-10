1881afe56c1d2f02c0a3ae1f353073e782f64f61
Andrey Borue <andrey.borue@gmail.com>
2018-02-10 16:02:48 +0200

    Fix

--- events/auth/js/AuthForm.js
+++ events/auth/js/AuthForm.js
@@ -1,46 +1,44 @@
 'use strict';
 
 const AuthForm = ({onAuth}) => {
-  let nameInput, passwordInput, emailInput;
+  let form;
 
   const onSubmit = event => {
     event.preventDefault();
     const data ={
-      name: nameInput.value,
-      password: passwordInput.value,
-      email: emailInput.value
+      name: form.elements['name'].value,
+      password: form.elements['password'].value,
+      email: form.elements['email'].value
     };
-
 
     typeof onAuth === 'function' ? onAuth(data) : console.error('onAuth не является функцией');
   };
 
-  const name = (
+  const checkPassword = event => {
+    event.currentTarget.value = event.currentTarget.value.replace(/[^a-zA-Z0-9_]/, '');
+  };
+
+  const checkEmail = event => {
+    event.currentTarget.value = event.currentTarget.value.replace(/[^a-zA-Z0-9_@\.\-]/, '');
+  };
+
+  return (
+    <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={onSubmit} ref={el => form = el} >
       <div className="Input">
-        <input required type="text" placeholder="Имя" ref={el => nameInput = el} />
+        <input required type="text" placeholder="Имя" name="name" />
         <label/>
       </div>
-  );
 
-  const password =  (
       <div className="Input">
-        <input required type="password" placeholder="Пароль" ref={el => passwordInput = el} onChange={event => {event.currentTarget.value = event.currentTarget.value.replace(/[^a-zA-Z0-9_]/, '')}} />
+        <input type="email" placeholder="Электронная почта" name="email" onChange={checkEmail} />
         <label />
       </div>
-  );
 
-  const email = (
-    <div className="Input">
-      <input type="email" placeholder="Электронная почта" ref={el => emailInput = el} onChange={event => {event.currentTarget.value = event.currentTarget.value.replace(/[^a-zA-Z0-9_@\.\-]/, '')}} />
+      <div className="Input">
+        <input required type="password" placeholder="Пароль"  name="password" onChange={checkPassword} />
         <label />
-    </div>
-  );
-  
-  return (
-    <form className="ModalForm" action="/404/auth/" method="POST" onSubmit={onSubmit}>
-      {name}
-      {email}
-      {password}
+      </div>
+
       <button type="submit">
         <span>Войти</span>
         <i className="fa fa-fw fa-chevron-right" />
--- events/feedback/js/FeedbackForm.js
+++ events/feedback/js/FeedbackForm.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const FeedbackForm = ({data, onSubmit}) => {
   const Fragment = React.Fragment;
   let form;
@@ -7,7 +6,6 @@ const FeedbackForm = ({data, onSubmit}) => {
   const Salutation = () => {
     const salutationList = ['Мистер', 'Мисис', 'Мис'].map((el, index) => {
       const id = "salutation-" + index;
-
       return (
         <Fragment>
           <input
@@ -17,7 +15,6 @@ const FeedbackForm = ({data, onSubmit}) => {
             id={id}
             value={el}
             defaultChecked={el === data.salutation}
-            onChange={el => el.currentTarget.checked ? data.salutation = el.currentTarget.value : true}
           />
           <label className="contact-form__label contact-form__label--radio" htmlFor={id}>{el}</label>
         </Fragment>
@@ -30,73 +27,6 @@ const FeedbackForm = ({data, onSubmit}) => {
     );
   };
 
-  const name = (
-    <div className="contact-form__input-group">
-      <label className="contact-form__label" htmlFor="name">Имя</label>
-      <input
-        className="contact-form__input contact-form__input--text"
-        id="name"
-        name="name"
-        type="text"
-        defaultValue={data.name}
-        onChange={(e) => data.name = e.currentTarget.value} />
-    </div>
-  );
-
-  const email = (
-    <div className="contact-form__input-group">
-      <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
-      <input
-        className="contact-form__input contact-form__input--email"
-        id="email"
-        name="email"
-        type="email"
-        defaultValue={data.email}
-        onChange={(e) => data.email = e.currentTarget.value} />
-    </div>
-  );
-
-  const help = (
-    <div className="contact-form__input-group">
-      <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
-      <select
-        className="contact-form__input contact-form__input--select"
-        id="subject"
-        name="subject"
-        defaultValue={data.subject}
-        onChange={(e) => data.subject = e.currentTarget.value}
-      >
-        <option>У меня проблема</option>
-        <option>У меня важный вопрос</option>
-      </select>
-    </div>
-  );
-
-  const message = (
-    <div className="contact-form__input-group">
-      <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
-      <textarea
-        className="contact-form__input contact-form__input--textarea"
-        id="message"
-        name="message"
-        rows="6"
-        cols="65"
-        defaultValue={data.message}
-        onChange={(e) => data.message = e.currentTarget.value}
-      />
-    </div>
-
-  );
-
-  const saveSnak = (e) => {
-    const item = e.currentTarget.value;
-    if (e.currentTarget.checked) {
-      data.snacks.push(item);
-    } else {
-      data.snacks.splice(data.snacks.indexOf(item), 1);
-    }
-  };
-
   const Snak = () => {
     const list = [{val: "пицца", el: "Пиццу"}, {val: "пирог", el: "Пирог"}].map((el, key) => {
       return (
@@ -106,7 +36,6 @@ const FeedbackForm = ({data, onSubmit}) => {
             id={"snacks-" + key}
             name="snacks"
             type="checkbox"
-            onChange={saveSnak}
             value={el.val}
             defaultChecked={data.snacks.indexOf(el.val) !== -1}
           />
@@ -123,15 +52,22 @@ const FeedbackForm = ({data, onSubmit}) => {
     );
   };
 
-  const sendButton = (
-    <button className="contact-form__button" type="submit">Отправить сообщение!</button>
-  );
-
   const funcOnSubmit = (event) => {
     event.preventDefault();
 
-    console.log(data);
-    console.log(JSON.stringify(data));
+    const snaks = [];
+    form.elements['snacks'].forEach(el => el.checked && snaks.push(el.value));
+
+
+    const data = {
+      salutation: form.elements['salutation'].value,
+      name: form.elements['name'].value,
+      subject: form.elements['subject'].value,
+      message: form.elements['message'].value,
+      email: form.elements['email'].value,
+      snacks: snaks
+    };
+
     onSubmit(JSON.stringify(data));
   };
 
@@ -145,15 +81,33 @@ const FeedbackForm = ({data, onSubmit}) => {
         <p>Чем мы можем помочь?</p>
       </div>
       <Salutation />
-      {name}
-      {email}
-      {help}
-      {message}
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="name">Имя</label>
+        <input className="contact-form__input contact-form__input--text" id="name" name="name" type="text" defaultValue={data.name} />
+      </div>
+
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="email">Адрес электронной почты</label>
+        <input className="contact-form__input contact-form__input--email" id="email" name="email" type="email" defaultValue={data.email} />
+      </div>
+
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="subject">Чем мы можем помочь?</label>
+        <select className="contact-form__input contact-form__input--select" id="subject" name="subject" defaultValue={data.subject}>
+          <option>У меня проблема</option>
+          <option>У меня важный вопрос</option>
+        </select>
+      </div>
+
+      <div className="contact-form__input-group">
+        <label className="contact-form__label" htmlFor="message">Ваше сообщение</label>
+        <textarea className="contact-form__input contact-form__input--textarea" id="message" name="message" rows="6" cols="65" defaultValue={data.message} />
+      </div>
+
       <Snak />
-      {sendButton}
+      <button className="contact-form__button" type="submit">Отправить сообщение!</button>
       <output id="result" />
     </form>
-    
   )
 };
 
