0acaa001e5961e3b1dc0148da8811a42eab36c3a
Andrey Borue <andrey.borue@gmail.com>
2018-03-11 20:38:20 +0200

    HOC

--- hoc/relative-time/js/DateTime.js
+++ hoc/relative-time/js/DateTime.js
@@ -5,3 +5,30 @@ const DateTime = props => {
         <p className="date">{props.date}</p>
     )
 };
+
+function DateTimeFormatter(Component) {
+    return class extends React.Component {
+        plural = (number, titles) => {
+            const cases = [2, 0, 1, 1, 1, 2];
+            return number + ' ' + titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+        };
+
+        render() {
+            const diffMinutes = (new Date() - new Date(this.props.date)) / 1000 / 60;
+
+            let result;
+            if (diffMinutes < 60) {
+                result = this.plural(diffMinutes, ['минута', 'минуты', 'минут']);
+            } else if (diffMinutes < 60*24) {
+                result = this.plural(Math.round(diffMinutes / 60), ['час', 'часа', 'часов']);
+            } else {
+                result = this.plural(Math.ceil(diffMinutes / 60 / 60), ['день', 'дня', 'дней']);
+            }
+            this.props.date = `${result}  назад`;
+
+            return <Component {...this.props} />;
+        }
+    }
+}
+
+const DateTimePretty = DateTimeFormatter(DateTime);
--- hoc/table-aggregation/js/App.js
+++ hoc/table-aggregation/js/App.js
@@ -1,5 +1,44 @@
 'use strict';
 
+function GroupAdapter(Component, groupBy) {
+  return class extends React.Component {
+    render() {
+      this.props.list = this.props.list.reduce(function(result, item) {
+        const date = new Date(item.date);
+        let number = (groupBy === 'month') ? date.getMonth() : date.getFullYear();
+
+        if (!(number in result)) {
+          result[number] = {"amount": 0};
+          result[number][groupBy] = (groupBy === 'month') ?  date.toLocaleString('en-us', { month: "short" }) : date.getFullYear();
+        }
+        result[number].amount += item.amount;
+
+        return result;
+      }, []);
+
+      return <Component {...this.props} />;
+    }
+  }
+}
+
+function SortAdapter(Component) {
+  return class extends React.Component {
+    render() {
+
+      this.props.list.sort(function(a ,b) {
+          // const aa = parseInt(a.date.replace('-', ''));
+          // console.log(a.date.replace('-', ''));
+          return parseInt(a.date.replace('-', '').replace('-', '')) - parseInt(b.date.replace('-', '').replace('-', ''));
+      });
+      return <Component {...this.props} />;
+    }
+  }
+}
+
+const MonthTableGroup = GroupAdapter(MonthTable, 'month');
+const YearTableGroup = GroupAdapter(YearTable, 'year');
+const SortTableGroup = SortAdapter(SortTable);
+
 class App extends React.Component {
     constructor(props) {
         super(props);
@@ -17,9 +56,9 @@ class App extends React.Component {
     render() {
         return (
             <div id="app">
-                <MonthTable list={this.state.list} />
-                <YearTable list={this.state.list} />
-                <SortTable list={this.state.list} />
+                <MonthTableGroup list={this.state.list} />
+                <YearTableGroup list={this.state.list} />
+                <SortTableGroup list={this.state.list} />
             </div>
         );
     }
